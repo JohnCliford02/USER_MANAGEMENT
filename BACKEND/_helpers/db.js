f6f86a6ae443ef8ea7ce38dc0a89db84a6b11824
Ashley Kyle Liao Tanglao <ashleykyletanglao@gmail.com>
2025-05-08 00:13:17 +0800

    Sir code

--- BACKEND/_helpers/db.js
+++ BACKEND/_helpers/db.js
@@ -18,26 +18,10 @@ async function initialize() {
   // init models and add them to the exported db object
   db.Account = require('../accounts/account.model')(sequelize);
   db.RefreshToken = require('../accounts/refresh-token.model')(sequelize);
-  db.Employee = require('../employees/employees.model')(sequelize);
-  db.Department = require('../departments/departments.model')(sequelize);
-  db.Request = require('../requests/request.model')(sequelize);
-  db.Workflow = require('../workflows/workflow.model')(sequelize);
 
   // define relationships
   db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
   db.RefreshToken.belongsTo(db.Account);
-
-  // Employee belongs to Department
-  db.Employee.belongsTo(db.Department, { foreignKey: 'departmentId', as: 'department' });
-  db.Department.hasMany(db.Employee, { foreignKey: 'departmentId', as: 'employees' });
-
-  //Workflow belongs to Employee
-   db.Workflow.belongsTo(db.Employee, { foreignKey: 'employeeId', as: 'employee' });
-   db.Employee.hasMany(db.Workflow, { foreignKey: 'employeeId', as: 'workflows' });
-
-  //Requests belongs to Employee
-  db.Request.belongsTo(db.Employee, { foreignKey: 'employeeId', as: 'employee' });
-  db.Employee.hasMany(db.Request, { foreignKey: 'employeeId', as: 'requests' });
 
   // sync all models with database
   await sequelize.sync({ alter: true });
